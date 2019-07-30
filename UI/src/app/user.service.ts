f7b81f5766dc3e94ee4424641debf3d9cfdb39dc
Ku CenturyLink <centurylink@airmail.cc>
2019-07-29 20:33:13 -0600

    WIP try to use observable

--- UI/src/app/user.service.ts
+++ UI/src/app/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class UserService {
   user: User;
   userUrl = "assets/user.json";
 
-  getAUser() {
-    return this.http.get(this.userUrl);
+  getAUser(): Observable<User> {
+    return this.http.get<User>(this.userUrl);
   }
 }
