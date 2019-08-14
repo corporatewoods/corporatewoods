e52bce6dec92ed14c58bc3f7e9d0e6a73f7939ad
Ku CenturyLink <centurylink@airmail.cc>
2019-08-13 20:10:30 -0600

    WIP use angular 8
    todo: fix styling, remove jquery, bootstrap
    go down the deep end with material design

--- dotnet/api/Startup.cs
+++ dotnet/api/Startup.cs
@@ -25,7 +25,7 @@ namespace api
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "Clientelle/dist";
             });
         }
 
@@ -59,7 +59,7 @@ namespace api
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "Clientelle";
 
                 if (env.IsDevelopment())
                 {
