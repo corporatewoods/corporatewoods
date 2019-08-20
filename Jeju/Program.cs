477f34e97d3c4439215338441a96dcde5ac2d123
Ku CenturyLink <centurylink@airmail.cc>
2019-08-20 17:19:06 -0600

    add a console project, jeju

---
+++ Jeju/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jeju
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
