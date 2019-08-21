cedb24c3d29b1a0b706cfc3551b697fcecb1c450
Ku CenturyLink <centurylink@airmail.cc>
2019-08-21 15:48:51 -0600

    add a new project Gimhae

---
+++ Gimhae/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gimhae.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
